2a04944b4482e65c79fc6929d2665e94a3eee6f3
fulechao <lcfu@netcast.com.cn>
2016-12-21 09:50:35 +0800

    BundleID和url修改

--- www/js/app.js
+++ www/js/app.js
@@ -8,8 +8,8 @@ var myApp = angular.module("myApp", ['ionic', 'ngCordova', 'ion-datetime-picker'
 
 //定义常量
 myApp.constant("ApiEndpoint", {
-  url: 'http://211.141.224.40:8070/mas_analysis',
-  //url: 'http://127.0.0.1:8080/mas_analysis',
+  // url: 'http://211.141.224.40:8070/mas_analysis',
+  url: 'http://10.1.1.91:8080/mas_analysis',
   //访问超时时间6s
   timeout: 6000
 });
